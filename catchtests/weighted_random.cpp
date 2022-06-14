34a44bef1c0e50b2ad5795364156b8b6c7bf9e8c
German Espinosa <germanespinosa@gmail.com>
2022-06-14 14:48:53 -0500

    stores frame number in stimulation step

--- catchtests/weighted_random.cpp
+++ catchtests/weighted_random.cpp
@@ -5,12 +5,28 @@
 using namespace cell_world;
 using namespace std;
 
-TEST_CASE("weighted_random") {
-    std::vector<float> weights{90,56,4};
-    std::vector<unsigned int> counters(3,0);
-    for (int i=0; i<150000000; i++){
-        counters[Chance::weighted_random(weights)]++;
-    }
-    for (auto c:counters)  cout << c << " ";
-    cout << endl;
+//TEST_CASE("weighted_random") {
+//    std::vector<float> weights{90,56,4};
+//    std::vector<unsigned int> counters(3,0);
+//    for (int i=0; i<150000000; i++){
+//        counters[Chance::weighted_random(weights)]++;
+//    }
+//    for (auto c:counters)  cout << c << " ";
+//    cout << endl;
+//}
+//
+//
+struct C : json_cpp::Json_object {
+    Json_object_members(
+            Add_optional_member(a);
+            Add_optional_member(b);
+            )
+    string a{};
+    int b{};
+};
+
+TEST_CASE("") {
+    json_cpp::Json_vector<C> vc;
+    "[{},{\"b\":1},{\"a\":\"a\"},{}]">>vc;
+    CHECK("[{},{\"b\":1},{\"a\":\"a\"},{}]" == vc.to_json());
 }

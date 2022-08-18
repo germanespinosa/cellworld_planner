25ff52b507cb9377c88abf153bc25b6b3a27b168
German Espinosa <germanespinosa@gmail.com>
2022-08-18 10:25:39 -0500

    show all stats

--- python/show_data.py
+++ python/show_data.py
@@ -25,27 +25,19 @@ for world_metric in world_stats:
     if isinstance(world_stats[world_metric], float):
         world_metrics.append(world_metric)
 
-def write_metrics(file, sim_type, clustering, world_number, entropy_bucket, stats_file, metrics, world_stats, world_metrics):
+def write_metrics(file, sim_type, branches, clustering, world_number, entropy_bucket, stats_file, metrics, world_stats, world_metrics):
     print(clustering, world_number, entropy_bucket, sim_type)
     simulation_statistics = vars(Simulation_statistics.load_from_file(stats_file))
-    file.write(sim_type)
-    file.write(",")
-    file.write(clustering)
-    file.write(",")
-    file.write(world_number)
-    file.write(",")
-    file.write(entropy_bucket)
+    line = "%s,%i,%s,%s,%s" % (sim_type, branches, clustering, world_number, entropy_bucket)
     for world_metric in world_metrics:
-        results_file.write(",")
-        results_file.write(str(world_stats[world_metric]))
+        line += ",%f" % (world_stats[world_metric],)
     for metric in metrics:
-        results_file.write(",")
-        results_file.write(str(simulation_statistics[metric]))
-    results_file.write("\n")
+        line += ",%f" % (simulation_statistics[metric],)
+    return line, simulation_statistics["success_rate"]
 
 
 with open("results.csv", "w") as results_file:
-    results_file.write("sim_type,clustering,world_number,entropy_bucket")
+    results_file.write("sim_type,branches_sampled,clustering,world_number,entropy_bucket")
 
     for world_metric in world_metrics:
         results_file.write(",")
@@ -54,7 +46,7 @@ with open("results.csv", "w") as results_file:
     for metric in metrics:
         results_file.write(",")
         results_file.write(metric)
-    results_file.write("\n")
+    results_file.write(",best_model_free_algorithm,planning_benefit,lppo_benefit\n")
 
     for x in glob.glob("*"):
         if os.path.isdir(x):
@@ -63,39 +55,68 @@ with open("results.csv", "w") as results_file:
             world = x.split(".")[1]
             world_number = world.split("_")[0]
             entropy_bucket = world.split("_")[1]
-            clustering = "no_limit"
-            if world_number[0] == 2:
-                clustering = "75%"
-            elif world_number[0] == 3:
-                clustering = "50%"
-            elif world_number[0] == 4:
-                clustering = "25%"
-            elif world_number[0] == 5:
-                clustering = "1"
+            clustering = "100% of occlusion count"
+            if world_number[0] == '2':
+                clustering = "75% of occlusion count"
+            elif world_number[0] == '3':
+                clustering = "50% of occlusion count"
+            elif world_number[0] == '4':
+                clustering = "25% of occlusion count"
+            elif world_number[0] == '5':
+                clustering = "random"
 
             world_stats = vars(World_statistics.load_from_file(x + "/world_statistics"))
+            branches = 0
+            lines = []
+            best_model_free_algorithm = ""
+            best_model_free_algorithm_success_rate = 0
+            lppo_success_rate = 0
+            pomcp_success_rate = 0
+            if exists(x+"/shortest_path_simulation_stats.json") and exists(x+"/shortest_path_simulation.json"):
+                branches = Simulation.load_from_file(x+"/shortest_path_simulation.json").parameters.tree_search_parameters.simulations
+                line, success_rate = write_metrics(results_file, "ASTAR", branches, clustering, world_number, entropy_bucket, x + "/shortest_path_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                if success_rate > best_model_free_algorithm_success_rate:
+                    best_model_free_algorithm_success_rate = success_rate
+                    best_model_free_algorithm = "ASTAR"
 
-            if exists(x + "/planning_simulation_stats.json"):
-                write_metrics(results_file, "POMCP", clustering, world_number, entropy_bucket, x + "/planning_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x + "/fixed_trajectory_simulation_stats.json") and exists(x + "/fixed_trajectory_simulation.json"):
+                line, success_rate = write_metrics(results_file, "FIXED_POMCP_TRAJECTORY", branches, clustering, world_number, entropy_bucket, x + "/fixed_trajectory_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                if success_rate > best_model_free_algorithm_success_rate:
+                    best_model_free_algorithm_success_rate = success_rate
+                    best_model_free_algorithm = "FIXED_POMCP_TRAJECTORY"
 
-            if exists(x + "/fixed_trajectory_simulation_stats.json"):
-                write_metrics(results_file, "FIXED_POMCP_TRAJECTORY", clustering, world_number, entropy_bucket, x + "/fixed_trajectory_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x + "/lppo_planning_simulation_stats.json") and exists(x + "/lppo_planning_simulation.json"):
+                line, success_rate = write_metrics(results_file, "LPPO", branches, clustering, world_number, entropy_bucket,  x + "/lppo_planning_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                lppo_success_rate = success_rate
 
-            if exists(x + "/lppo_planning_simulation_stats.json"):
-                write_metrics(results_file, "LPPO", clustering, world_number, entropy_bucket, x + "/lppo_planning_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x+"/fixed_lppo_trajectory_simulation_stats.json") and exists(x+"/fixed_lppo_trajectory_simulation.json"):
+                line, success_rate = write_metrics(results_file, "FIXED_LPPO_TRAJECTORY", branches, clustering, world_number, entropy_bucket,  x + "/fixed_lppo_trajectory_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                if success_rate > best_model_free_algorithm_success_rate:
+                    best_model_free_algorithm_success_rate = success_rate
+                    best_model_free_algorithm = "FIXED_LPPO_TRAJECTORY"
 
-            if exists(x+"/fixed_lppo_trajectory_simulation_stats.json"):
-                write_metrics(results_file, "FIXED_LPPO_TRAJECTORY", clustering, world_number, entropy_bucket, x + "/fixed_lppo_trajectory_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x+"/thigmotaxis_simulation_stats.json") and exists(x+"/thigmotaxis_simulation.json"):
+                line, success_rate = write_metrics(results_file, "THIGMOTAXIS", branches, clustering, world_number, entropy_bucket, x + "/thigmotaxis_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                if success_rate > best_model_free_algorithm_success_rate:
+                    best_model_free_algorithm_success_rate = success_rate
+                    best_model_free_algorithm = "THIGMOTAXIS"
 
-            if exists(x+"/thigmotaxis_simulation_stats.json"):
-                write_metrics(results_file, "THIGMOTAXIS", clustering, world_number, entropy_bucket, x + "/thigmotaxis_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x+"/reactive_thigmotaxis_simulation_stats.json") and exists(x+"/reactive_thigmotaxis_simulation.json"):
+                line, success_rate = write_metrics(results_file, "REACTIVE_THIGMOTAXIS", branches, clustering, world_number, entropy_bucket, x + "/reactive_thigmotaxis_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                if success_rate > best_model_free_algorithm_success_rate:
+                    best_model_free_algorithm_success_rate = success_rate
+                    best_model_free_algorithm = "REACTIVE_THIGMOTAXIS"
 
-            if exists(x+"/reactive_thigmotaxis_simulation_stats.json"):
-                write_metrics(results_file, "REACTIVE_THIGMOTAXIS", clustering, world_number, entropy_bucket, x + "/reactive_thigmotaxis_simulation_stats.json", metrics, world_stats, world_metrics)
+            if exists(x + "/planning_simulation_stats.json") and exists(x + "/planning_simulation.json"):
+                line, success_rate = write_metrics(results_file, "POMCP", branches, clustering, world_number, entropy_bucket, x + "/planning_simulation_stats.json", metrics, world_stats, world_metrics)
+                lines.append(line)
+                pomcp_success_rate = success_rate
 
-            if exists(x+"/shortest_path_simulation_stats.json"):
-                write_metrics(results_file, "ASTAR", clustering, world_number, entropy_bucket, x + "/shortest_path_simulation_stats.json", metrics, world_stats, world_metrics)
-
-
-
-
+            completed_lines = [line + ",%s,%f,%f\n" % (best_model_free_algorithm, pomcp_success_rate - best_model_free_algorithm_success_rate, lppo_success_rate - best_model_free_algorithm_success_rate) for line in lines]
+            results_file.writelines(completed_lines)

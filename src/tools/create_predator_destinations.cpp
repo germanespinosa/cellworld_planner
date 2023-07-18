19bf9a045e2f9d756341fddbb497ef4f92e373da
German Espinosa <germanespinosa@gmail.com>
2023-07-18 14:07:34 -0500

    fixed predator destinations to remove cell near occlusions or walls

--- src/tools/create_predator_destinations.cpp
+++ src/tools/create_predator_destinations.cpp
@@ -13,9 +13,10 @@ int main (int argc, char **argv){
     World world = World::get_from_parameters_name("hexagonal","canonical", occlusions);
     Graph graph = world.create_graph();
     Cell_group cells = world.create_cell_group().free_cells();
+    Graph g = world.create_graph();
     Cell_group pd;
     for (const Cell &cell:cells) {
-        if (graph.is_connected(cell,world.cells[0])){
+        if (graph.is_connected(cell,world.cells[0]) && g[cell].size() == world.connection_pattern.size()){
             pd.add(cell);
         }
     }

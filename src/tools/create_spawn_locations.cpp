1a497ef467e028067992db773eec4b01fd6b18b1
German Espinosa <germanespinosa@gmail.com>
2023-07-18 14:06:38 -0500

    fixed spawn locations to remove cell near occlusions or walls

--- src/tools/create_spawn_locations.cpp
+++ src/tools/create_spawn_locations.cpp
@@ -15,6 +15,7 @@ int main (int argc, char **argv){
 
     auto cells = world.create_cell_group();
     Cell_group pd = world.create_cell_group(Resources::from("cell_group").key("hexagonal").key(occlusions).key("predator_destinations").get_resource<Cell_group_builder>());
+    Graph g = world.create_graph();
     Graph visibility = world.create_graph(Resources::from("graph").key("hexagonal").key(occlusions).key("cell_visibility").get_resource<Graph_builder>());
     Cell_group spawn_locations;
     Map map(cells);
@@ -22,7 +23,7 @@ int main (int argc, char **argv){
     auto &start_location_visibility = visibility[start_location];
     for (const Cell &cell:pd) {
         if (cell.location.dist(start_location.location) > threshold &&
-                !start_location_visibility.contains(cell)) {
+                !start_location_visibility.contains(cell) && g[cell].size() == world.connection_pattern.size()) {
             spawn_locations.add(cell);
         }
     }
